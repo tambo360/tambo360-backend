f3f3570cae9cfe94f0185cfe9f0975ef0af67db0
Facundo Fernández <facuu201202@gmail.com>
2026-05-26 18:22:21 -0300

    feat: add authentication and organization access middleware to mermas routes

--- src/routes/mermas.ts
+++ src/routes/mermas.ts
@@ -1,7 +1,15 @@
 import { Router } from "express"
 import  MermaController from "../controllers/mermaController"
+import { establecimientoRequireOrgAccess, estContext, orgContext, requireOrgAccess } from "../middleware/orgMiddleware";
+import { authenticate } from "../middleware/authMiddleware";
 
 const router = Router()
+
+router.use(authenticate);
+router.use(orgContext);
+router.use(requireOrgAccess);
+router.use(estContext);
+router.use(establecimientoRequireOrgAccess);
 
 router.get("/tipos", MermaController.getTipos)
 router.post("/", MermaController.create)
